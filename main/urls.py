a70e06f45f12116b07e88413eed0380aab8b017e
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-09-13 20:41:20 +0700

    Menambahkan 4 fungsi views baru untuk melihat objek yang sdh ditambahkan, Membuat routing URL, membuat halaman dan tombol Add

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_main, show_products, show_about, product_detail
+from .views import show_main, show_products, show_about, product_detail, add_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
@@ -8,4 +8,9 @@ urlpatterns = [
     path('products/', show_products, name='show_products'),
     path('about/', show_about, name='show_about'),
     path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('add-product/', add_product, name='add_product'),
+    path('xml/', show_xml, name='show_xml'), 
+    path('json/', show_json, name='show_json'), 
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Product
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.core import serializers
+from .forms import ProductForm
 
 def show_main(request):
     products = Product.objects.filter(is_featured=True) 
@@ -25,3 +28,28 @@ def product_detail(request, pk):
         'product': product
     }
     return render(request, 'product_detail.html', context)
+
+def add_product(request):
+    form = ProductForm()
+    context = {'form': form}
+    return render(request, 'add_product.html', context)
+
+def show_xml(request):
+    products = Product.objects.all()
+    data = serializers.serialize('xml', products)
+    return HttpResponse(data, content_type="application/xml")
+
+def show_json(request):
+    products = Product.objects.all()
+    data = serializers.serialize('json', products)
+    return HttpResponse(data, content_type="application/json")
+
+def show_xml_by_id(request, id):
+    product = Product.objects.filter(pk=id)
+    data = serializers.serialize('xml', product)
+    return HttpResponse(data, content_type="application/xml")
+
+def show_json_by_id(request, id):
+    product = Product.objects.filter(pk=id)
+    data = serializers.serialize('json', product)
+    return HttpResponse(data, content_type="application/json")
