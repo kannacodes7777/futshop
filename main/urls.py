077ea590234203bf528fd1bee4e6a3d470fc51b3
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-09-15 20:35:57 +0700

    Fix problem pk dan id mismatch

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('add-product/', add_product, name='add_product'),
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
-    path('xml/<uuid:pk>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<uuid:pk>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
 ]
