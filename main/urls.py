23a84ca12873f176c3b1f44d3d8e5d1e258cbdca
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-09-15 14:05:14 +0700

    Migrate dari id int ke uuid

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('add-product/', add_product, name='add_product'),
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<uuid:pk>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<uuid:pk>/', show_json_by_id, name='show_json_by_id'),
 ]
