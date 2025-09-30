177f4066c5978aa93ea735e63793fc3ceeaf1577
Hillary Elizabeth <hillary.elizabeth@ui.ac.id>
2025-09-30 23:18:38 +0700

    Add static

--- futshop/wsgi.py
+++ futshop/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# futshop/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'futshop.settings')
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
